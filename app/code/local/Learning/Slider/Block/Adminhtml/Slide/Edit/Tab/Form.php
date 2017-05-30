8cdc2e2d7675f1f30cfa77e944e9f0695bf3de2a
Fabian Sebban <fa.sebban@gmail.com>
2017-05-30 10:51:52 +0200

    Saving slide image

--- app/code/local/Learning/Slider/Block/Adminhtml/Slide/Edit/Tab/Form.php
+++ app/code/local/Learning/Slider/Block/Adminhtml/Slide/Edit/Tab/Form.php
@@ -9,7 +9,7 @@ class Learning_Slider_Block_Adminhtml_Slide_Edit_Tab_Form extends Mage_Adminhtml
         $this->setForm($form);
         $fieldset = $form->addFieldset('slide_form', array('legend' => Mage::helper('learning_slider')->__('Slide information')));
 
-        //$fieldset->addType('image', 'Learning_Slider_Block_Adminhtml_Form_Renderer_Image');
+        $fieldset->addType('image', 'Learning_Slider_Block_Adminhtml_Form_Renderer_Image');
         $fieldset->addField('name', 'text', array(
             'label'    => Mage::helper('learning_slider')->__('Name'),
             'name'     => 'name',
@@ -17,12 +17,12 @@ class Learning_Slider_Block_Adminhtml_Slide_Edit_Tab_Form extends Mage_Adminhtml
             'required' => true
         ));
 
-        /*$fieldset->addField('image_url', 'image', array(
+        $fieldset->addField('image_url', 'image', array(
             'label'     => Mage::helper('learning_slider')->__('Image'),
             'required'  => false,
             'name'      => 'image_url',
             'directory' => 'slide/'
-        ));*/
+        ));
 
         $fieldset->addField('is_active', 'select', array(
             'label'    => Mage::helper('learning_slider')->__('Status'),
