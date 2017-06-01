28512f12de27d7a4a8b1488028b1682cf16ec7e0
ghansum <ghansumn@gmail.com>
2017-06-01 12:10:32 +0200

    of shouf ehhh produit

--- app/code/local/Learning/BrandPackage/Helper/Data.php
+++ app/code/local/Learning/BrandPackage/Helper/Data.php
@@ -2,5 +2,22 @@
 
 class Learning_BrandPackage_Helper_Data extends Mage_Core_Helper_Abstract
 {
+  const IMAGE_FOLDER = "product";
 
+  /**
+   * Renvoie l'URL de l'image
+   * @param $filename
+   * @return string
+   */
+  public function getImageUrl($filename)
+  {
+      return Mage::getBaseUrl('media') . self::IMAGE_FOLDER . '/' . $filename;
+  }
+
+  public function getProductImageUrl($product)
+  {
+      $imageUrl = Mage::getBaseUrl('media') . 'catalog/product' . $product->getImage();
+      $imageCacheUrl = Mage::helper('catalog/image')->init($product, 'image');
+      return $imageCacheUrl;
+  }
 }
